3ce1b436cb171805f1b76c2478e18349f11cc904
Greg Junge <gregnuj@gmail.com>
2020-09-10 14:48:32 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,11 @@ if [[ "${DEBUG}" -eq "true" ]]; then
 fi
 
 GIT_USERNAME=${INPUT_GIT_USERNAME:-${GIT_USERNAME:-"git"}}
-REMOTE=${INPUT_REMOTE:-"$*"}
 GIT_SSH_PRIVATE_KEY=${INPUT_GIT_SSH_PRIVATE_KEY}
-GIT_SSH_PUBLIC_KEY=${INPUT_GIT_SSH_PUBLIC_KEY}
 GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--tags --force --prune"}
-GIT_SSH_NO_VERIFY_HOST=${INPUT_GIT_SSH_NO_VERIFY_HOST}
-GIT_SSH_KNOWN_HOSTS=${INPUT_GIT_SSH_KNOWN_HOSTS}
+REMOTE=${INPUT_REMOTE:-"$*"}
+SOURCE_BRANCH=${INPUT_SOURCE_BRANCH:-"*"}
+REMOTE_BRANCH=${INPUT_REMOTE_BRANCH:-"${SOURCE_BRANCH}"}
 HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2>/dev/null || /bin/true)"
 
 
@@ -34,28 +33,10 @@ fi
 git config --global credential.username "${GIT_USERNAME}"
 
 
-if [[ "${GIT_SSH_PRIVATE_KEY}" != "" ]]; then
-    mkdir ~/.ssh
-    chmod 700 ~/.ssh
-    echo "${GIT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
-    if [[ "${GIT_SSH_PUBLIC_KEY}" != "" ]]; then
-        echo "${GIT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
-        chmod 600 ~/.ssh/id_rsa.pub
-    fi
+if [[ "${GIT_SSH_PRIVATE_KEY}" -ne "" ]]; then
+    mkdir -p ~/.ssh
+    echo "${INPUT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
-    if [[ "${GIT_SSH_KNOWN_HOSTS}" != "" ]]; then
-      echo "${GIT_SSH_KNOWN_HOSTS}" > ~/.ssh/known_hosts
-      git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts"
-    else
-      if [[ "${GIT_SSH_NO_VERIFY_HOST}" != "true" ]]; then
-        echo "WARNING: no known_hosts set and host verification is enabled (the default)"
-        echo "WARNING: this job will fail due to host verification issues"
-        echo "Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs"
-        exit 1
-      else
-        git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-      fi
-    fi
 else
     git config --global core.askPass /cred-helper.sh
     git config --global credential.helper cache
@@ -63,13 +44,4 @@ fi
 
 
 git remote add mirror "${REMOTE}"
-if [[ "${INPUT_PUSH_ALL_REFS}" != "false" ]]; then
-    eval git push ${GIT_PUSH_ARGS} mirror "\"refs/remotes/origin/*:refs/heads/*\""
-else
-    if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
-        echo "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch" > /dev/stderr
-        exit 1
-    else
-        eval git push -u ${GIT_PUSH_ARGS} mirror
-    fi
-fi
+eval git push ${GIT_PUSH_ARGS} mirror "\"refs/remotes/origin/${SOURCE_BRANCH}:refs/heads/{$REMOTE_BRANCH}\""
